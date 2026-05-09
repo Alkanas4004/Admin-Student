e0c55aec7f3cac6dfb5359692fe64d44fafc8295
Alkanas4004 <thestormleaksgang555@gmail.com>
2026-05-09 18:12:28 +0300

    Add user authentication and role-based redirection

--- scripts/guard.js
+++ scripts/guard.js
@@ -1,1 +1,13 @@
 
+<script type="module">
+const user = JSON.parse(localStorage.getItem("user"));
+
+if (!user) {
+  window.location.href = "login.html";
+}
+
+// لو طالب دخل dashboard
+if (location.pathname.includes("dashboard") && user.role !== "admin") {
+  window.location.href = "index.html";
+}
+</script>
